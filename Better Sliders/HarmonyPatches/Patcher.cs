8bb15ef5aeb7ee1c32a2a254af09b6bca169c5b1
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2022-01-11 14:25:01 -0600

    Moved the Harmony patching class to its own separate file.

--- Better Sliders/HarmonyPatches/Patcher.cs
+++ Better Sliders/HarmonyPatches/Patcher.cs
@@ -1,17 +1,17 @@
 ﻿// MIT License
-//
-// Copyright (c) 2021 SirRandoo
-//
+// 
+// Copyright (c) 2022 SirRandoo
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-//
+// 
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,27 +23,17 @@
 using System.Reflection;
 using HarmonyLib;
 using JetBrains.Annotations;
-using UnityEngine;
 using Verse;
 
-namespace SirRandoo.BetterSliders
+namespace SirRandoo.BetterSliders.HarmonyPatches
 {
     [UsedImplicitly]
-    public class BetterSliders : Mod
+    [StaticConstructorOnStartup]
+    public static class Patcher
     {
-        public BetterSliders(ModContentPack content) : base(content)
+        static Patcher()
         {
-            GetSettings<SliderSettings>();
-        }
-
-        public override string SettingsCategory()
-        {
-            return Content.Name;
-        }
-
-        public override void DoSettingsWindowContents(Rect region)
-        {
-            SliderSettings.Draw(region);
+            new Harmony("com.sirrandoo.bettersliders").PatchAll(Assembly.GetExecutingAssembly());
         }
     }
 }
